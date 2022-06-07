09c172ed41083270269540708821c55f77e90ea7
lichee55 <lichee55@gmail.com>
2022-06-07 11:30:39 +0900

    [add] project files

---
+++ src/main/java/com/kit/kumovie/KumovieApplication.java
@@ -0,0 +1,13 @@
+package com.kit.kumovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KumovieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KumovieApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kit/kumovie/KumovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kit.kumovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KumovieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
